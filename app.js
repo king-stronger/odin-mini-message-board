fcd3b92f90773af6c0d762b2611cf2e676e7bf46
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-01-23 23:45:21 +0000

    Set the express.urlencoded in app.js and use the router from router.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import url from "url";
 import path from "path";
 import express from "express";
+import { router } from "./router.js";
 
 const app = express();
 
@@ -11,10 +12,9 @@ const dirname = path.dirname(filename);
 app.use(express.static(path.join(dirname, "public")));
 app.set("views", path.join(dirname, "views"));
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true}))
 
-app.get("/", (req, res) => {
-    res.render("index", { message: "You're beautiful 1 It's true !"})
-});
+app.use(router)
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
